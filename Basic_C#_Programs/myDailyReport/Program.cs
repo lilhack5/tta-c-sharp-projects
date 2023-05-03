ba5b9e9401fcdfc46737da2eba4aa66bd0dc3c9c
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2023-05-03 09:57:33 -0700

    changed int to string and casted the changes
    changed the bool to if statement

--- Basic_C#_Programs/myDailyReport/Program.cs
+++ Basic_C#_Programs/myDailyReport/Program.cs
@@ -19,20 +19,29 @@ namespace myDailyReport
             Console.WriteLine("I am on course " + course + ".");// Show what course the user inputed
 
             Console.WriteLine("What page number?");// Asks the user what page number they are on
-            int page = Console.ReadLine();// Allows the user to input the page they are on
-            Console.WriteLine("The page is " + page + ".");// Shows what page the user inputed
+            string page = Console.ReadLine();// Allows the user to input the page they are on
+            int pageNum = Convert.ToInt32(page);
+            Console.WriteLine("The page is " + pageNum + ".");// Shows what page the user inputed
 
-            Console.WriteLine("Do you need help with anything? Please answer true or false.");//Ask the user if they need any help.
-            bool help = Console.ReadLine();// Allows the user to input true or false
-            Console.WriteLine("Do I need help? " + help);// shows if the user needs help or not
-            
+            string needHelp = true; //Creates variable needHelp
+
+            if (needHelp)// if they need help then write you need help.
+                { 
+                    Console.WriteLine("Yes, I do need some help.");
+            }
+            else // reads that you do not need help
+                {
+                    Console.WriteLine("No, I do not need any help.");
+            }
+
             Console.WriteLine("Were there any positive feedback you'd like to provide? Please give specifics");// Asks user if there were any feedback they would like to give
             string feedback = Console.ReadLine();// Allows the user to input their feedback
             Console.WriteLine(feedback);// Shows the users feedback
 
             Console.WriteLine("How many hours did you study?");// Asks user how many hours they studied
-            int study = Console.ReadLine();// Allows user to input the hours studied
-            Console.WriteLine("I studied for " + study + " hours.");// Shows how many hours the user studied
+            string study = Console.ReadLine();// Allows user to input the hours studied
+            int studyHours = Convert.ToInt32(study);
+            Console.WriteLine("I studied for " + studyHours + " hours.");// Shows how many hours the user studied
 
             Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");// After the user answers how long they studied this will show below hours studied
 
