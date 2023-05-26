87191cbaf5c9e7f9356baa277dfebcb5ab00aec9
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2023-05-26 11:41:45 -0700

    deleted and did arrays over
    needed to start over

--- Basic_C#_Programs/arrays/Program.cs
+++ Basic_C#_Programs/arrays/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace arrays
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Please pick 1 of these numbers: 0, 1, 2");// when the program starts this is the first line that will appear.
+            string[] names = { "Billy", "Bob", "Joe" };// created a string array of names.
+            string pick = Console.ReadLine();//
+            int pickNum = Convert.ToInt32(pick);
+            Console.WriteLine(names[pickNum]);
+
+            Console.WriteLine("Please pick 1 of these numbers: 0, 1, 2");//this will show up after the first question has been answered.
+            int[] num = { 5, 55, 555 };//created an array of integers.
+            int number = Convert.ToInt32(Console.ReadLine());//allows the user to pick a number and converts it from a string to an integer.
+            
+            if (number < 3)
+            {
+                Console.WriteLine(num[number]);//will show one of thee integers from the array.
+            }
+            else
+            {
+                Console.WriteLine("You picked a wrong number.");//will show if the user picked a number outside of the range set.
+            }
+
+            Console.WriteLine("Please pick 1 of these numbers: 0, 1, 2");//this will show up after the second question has been answered.
+            List<string> intList = new List<string>();//created a new list
+            intList.Add("Baseball");//added to the list.
+            intList.Add("Apex");
+            intList.Add("Sleep");
+            int listNum = Convert.ToInt32(Console.ReadLine());//allows user to pick a number for the index.
+            Console.WriteLine(intList[listNum]);//will show what is in the index spot that the user picked
+
+
+
+            Console.Read();
         }
     }
 }
