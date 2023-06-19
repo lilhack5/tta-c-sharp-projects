f26efae9a8f1dc4bed71792469215c82688f4835
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2023-06-19 12:48:41 -0700

    fixed some list issues
    got the list contained

--- Basic_C#_Programs/iteration/Program.cs
+++ Basic_C#_Programs/iteration/Program.cs
@@ -11,17 +11,17 @@ namespace iteration
             string[] names = { "Billy", "Bob", "Joe" };// created a string of names
             Console.WriteLine("Please enter a name.");//this will show when you start the program.
             string name = Console.ReadLine();
-            for(int h = 0; h<names.Length; h++)
+            for (int h = 0; h < names.Length; h++)
             {
                 names[h] += name;//
             }
-           
-            
-            for(int w = 0; w < names.Length; w++)
+
+
+            for (int w = 0; w < names.Length; w++)
             {
                 Console.WriteLine(names[w]);//prints the list of the array to the console.
             }
-            
+
 
 
             for (int i = 0; i < 5; i++)// had i decreasing and it starting at 5 and going down it will never reach 5 so switched i-- to i++.
@@ -38,42 +38,61 @@ namespace iteration
             Console.WriteLine("Please write your favorite streamer.");//allows the user to enter text on the console
             string apex = Console.ReadLine();//allows the user to enter a streamer
 
-            foreach (string player in players)           
+            for (int i = 0; i < players.Count; i++)
             {
-                Console.WriteLine(player);//shows index on the console
-            }
+                if (players[i] == apex)
+                {
+                    Console.WriteLine("You used text already in the index" + i);// writes to the console they used text and the position in the list.
+                }
 
-            if (apex != players[0] || apex != players[1] || apex != players[2])
-            {            
-                Console.WriteLine("The text you input is not on the list.");//shows if the users input is not on the list
-            }
+                else if (!players.Contains(apex))
+                {
+                    Console.WriteLine("Your input is not on the list");
+                    break;//pulls out of the loop
+                }
+            }           
 
-            else
-            {
-                Console.WriteLine("You used text already in the index");                    
-            }
-            
             List<string> notcreative = new List<string>() { "Jesus loves you.", "Jesus loves you." };//Created identical strings.
             Console.WriteLine("Please add text to search the list.");// shows on the console
             string notCreative = Console.ReadLine();//allows the user to enter text
 
-            if (notCreative == notcreative[0] || notCreative == notcreative[1])
+            for (int t = 0; t < notcreative.Count; t++)
             {
-                Console.WriteLine(notCreative);//if the users' text matches the indices it will show on the console.
+                if (notcreative[t] == notCreative)
+                {
+                    Console.WriteLine(t);// will show what inidici the text is on
+                }
+
+                else if (!notcreative.Contains(notCreative))
+                {
+                    Console.WriteLine("Your input is not on the list");// will show that your item is not on the list
+                }
             }
 
-            if (notCreative != notcreative[0] || notCreative != notcreative[1])
-            {
-                Console.WriteLine("Your input is not on the list.");//if the text the user input does not match this will show
-            }
 
-            List<string> saves = new List<string>() { "Jesus saves.", "Jesus saves." };//created indetical strings.
-            
+
+
+            List<string> saves = new List<string>() { "Jesus is how to get to heaven", "Jesus saves.", "Jesus saves." };//created lindetical strings.
+            List<string> heaven = new List<string>();//created an empty list
+
+
+            for (int h = 0; h < saves.Count; h++)
             foreach (string save in saves)
             {
-                if (save == saves[0] || save == saves[1])
-                    Console.WriteLine("This item in a duplicate.");//shows on the console if the text is the same.
+                if (!saves[h].Contains(save))//if the current element in the original list is contained in the second list
+
+                {
+                    Console.WriteLine(saves[h] + " is a duplicate.");
+                }
+
+                else
+                {
+                    Console.WriteLine(saves[h] + " is unique.");
+                    heaven.Add(saves[h]);
+                }
             }
+
+
 
 
 
