e5b3c49af678a3ed8530c7f8aefc781020ee7404
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2023-06-22 10:00:04 -0700

    created a method
    created math method

---
+++ Basic_C#_Programs/methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to do the math operations on?");//shows on the first line of the console.
+            int numUser = Convert.ToInt32(Console.Read());//allows the user to enter a number and turn it from a string to an integer.
+
+            math mathOne = new math();
+            mathOne.jesus = +5;
+            mathOne.loves = -70;
+            mathOne.you = +25;
+
+            Console.WriteLine(numUser + mathOne.jesus);// takes what the user typed in and adds to mathOne.jesus
+            Console.ReadLine();
+            Console.WriteLine(numUser + mathOne.loves);//takes what the user typed and adds to mathOne.loves
+            Console.ReadLine();
+            Console.WriteLine(numUser + mathOne.you);//takes what the user typed and adds to mathOne.you
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/methods/math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    public class math
+    {
+        public math()
+        {
+            jesus = 5;
+            loves = 70;
+            you = 25;
+
+        }
+
+        public int jesus { get; set; }
+        public int loves { get; set; }
+        public int you { get; set; }
+
+    }
+}
